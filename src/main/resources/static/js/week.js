925eef14dcab26ef882787e273c1da123e1e8db8
Joachim <kirviq@users.noreply.github.com>
2019-12-24 14:05:13 +0100

    swipe to switch weeks

--- src/main/resources/static/js/week.js
+++ src/main/resources/static/js/week.js
@@ -1,37 +1,78 @@
 
 (function() {
-	function getDataFromButton(el) {
-		while (el.tagName != 'BUTTON') {
-			el = el.parentNode;
+	(function handleSwipes() {
+		function getCurrentWeeksPast() {
+			if (!location.search || location.search.indexOf('weeksPast') == -1) {
+				return 0;
+			}
+			return parseInt(location.search.replace(/^.*weeksPast=(\d+).*/, '$1'));
 		}
-		return new Proxy({}, {
-			get(target, property) {
-				property = property.replace(/([A-Z])/g, '-$1').toLowerCase();
-				return el.getAttribute(`data-${property}`);
-			},
-			set(target, property, value) {
-				property = property.replace(/-[a-z]/g, string => string.substring(1).toUpperCase());
-				el.setAttribute(`data-${property}`, value);
+		let startX, startY;
+		let lastX, lastY;
+		document.addEventListener('touchstart', event => {
+			startX = event.touches[0].clientX;
+			startY = event.touches[0].clientY;
+		});
+		document.addEventListener('touchmove', event => {
+			lastX = event.touches[0].clientX;
+			lastY = event.touches[0].clientY;
+		});
+		document.addEventListener('touchend', event => {
+			const moveX = lastX - startX;
+			const moveY = lastY - startY;
+			if (moveY > 50) {
+				return;
+			}
+			if (moveX < -50) {
+				const currentWeek = getCurrentWeeksPast();
+				if (currentWeek === 0) {
+					return;
+				}
+				location.search = `weeksPast=${currentWeek - 1}`
+			}
+			if (moveX > 50) {
+				location.search = `weeksPast=${getCurrentWeeksPast() + 1}`
 			}
 		});
-	}
-	function revealStuff(event) {
-		const data = getDataFromButton(event.target);
-		document.getElementById(data.showId).className += ' visible';
-	}
-	function hideForm(event) {
-		const form = event.target;
-		if (form.tagName != 'FORM') {
-			return;
+	})();
+
+	(function handlePopupForms() {
+		function getDataFromButton(el) {
+			while (el.tagName != 'BUTTON') {
+				el = el.parentNode;
+			}
+			return new Proxy({}, {
+				get(target, property) {
+					property = property.replace(/([A-Z])/g, '-$1').toLowerCase();
+					return el.getAttribute(`data-${property}`);
+				},
+				set(target, property, value) {
+					property = property.replace(/-[a-z]/g, string => string.substring(1).toUpperCase());
+					el.setAttribute(`data-${property}`, value);
+				}
+			});
 		}
-		form.className = form.className.replace(/visible/g, '');
-	}
-	addEventListener('DOMContentLoaded', () => {
-		Array.prototype.forEach.call(document.querySelectorAll('.revealer-button'), button => {
-			button.addEventListener('click', revealStuff, false);
+
+		function revealStuff(event) {
+			const data = getDataFromButton(event.target);
+			document.getElementById(data.showId).className += ' visible';
+		}
+
+		function hideForm(event) {
+			const form = event.target;
+			if (form.tagName != 'FORM') {
+				return;
+			}
+			form.className = form.className.replace(/visible/g, '');
+		}
+
+		addEventListener('DOMContentLoaded', () => {
+			Array.prototype.forEach.call(document.querySelectorAll('.revealer-button'), button => {
+				button.addEventListener('click', revealStuff, false);
+			});
+			Array.prototype.forEach.call(document.querySelectorAll('.fullscreen-form'), button => {
+				button.addEventListener('click', hideForm, false);
+			});
 		});
-		Array.prototype.forEach.call(document.querySelectorAll('.fullscreen-form'), button => {
-			button.addEventListener('click', hideForm, false);
-		});
-	});
+	})();
 })();
